716497671f277f175fba6b2008ab4f0bb9094653
Kodjo Akator <koak0001@stud.kea.dk>
2024-05-02 13:41:34 +0200

    Initial commit

---
+++ src/main/java/projectcalculationtool/ProjectCalculationToolApplication.java
@@ -0,0 +1,13 @@
+package projectcalculationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalculationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCalculationToolApplication.class, args);
+	}
+
+}
