346864acc6aeaa55dcf01e891cdfc6c64668454f
Kodjo Akator <koak0001@stud.kea.dk>
2024-05-08 10:50:51 +0200

    Several methods completed for testing purposes

--- src/main/java/projectcalculationtool/model/Project.java
+++ src/main/java/projectcalculationtool/model/Project.java
@@ -4,7 +4,8 @@ import java.util.Date;
 
 public class Project {
     private String projectName;
-    private int projectID;
+    private String userRole;
+    private int projectId;
     private Date deadline;
     private int totalHours;
     boolean isArchived;
@@ -12,7 +13,35 @@ public class Project {
     public Project(String projectName) {
         this.projectName = projectName;
     }
+    public String getProjectName() {
+        return projectName;
+    }
+    public String getUserRole() {
+        return userRole;
+    }
 
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public boolean isArchived() {
+        return isArchived;
+    }
     public void setArchived(boolean archived) {
         isArchived = archived;
     }
--- src/main/java/projectcalculationtool/model/Role.java
+++ src/main/java/projectcalculationtool/model/Role.java
@@ -3,9 +3,8 @@ package projectcalculationtool.model;
 public class Role {
     private String title;
     private int roleId;
-    public Role(String title, int roleId) {
+    public Role(String title) {
         this.title = title;
-        this.roleId = roleId;
     }
     public String getTitle() {return title;}
     public int getRoleId() {return roleId;}
