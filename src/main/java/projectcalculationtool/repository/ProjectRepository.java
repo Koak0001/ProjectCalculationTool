0e08877489502b3cf1d34b8f40bb2e151db7f80a
Kodjo Akator <koak0001@stud.kea.dk>
2024-05-11 18:20:46 +0200

    Repository method getSubProjects, now lists each subprjects required hours, based on the 'hours' listed on its associated tasks.

--- src/main/java/projectcalculationtool/repository/ProjectRepository.java
+++ src/main/java/projectcalculationtool/repository/ProjectRepository.java
@@ -20,120 +20,124 @@ public class ProjectRepository {
     String dbPassword;
 
 
-
 //    TODO - login/verifyUser
 //    TODO  - logout
 
-//    getProjects
-public List<Project> getProjects(int userId) {
-    List<Project> projects = new ArrayList<>();
-    try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
-        String sql = "SELECT P.projectId, P.projectName, R.RoleTitle " +
-                "FROM User_Project_Role UPR " +
-                "JOIN Project P ON UPR.ProjectID = P.ProjectID " +
-                "JOIN Role R ON UPR.RoleID = R.RoleID " +
-                "WHERE UPR.UserID = ?";
-        PreparedStatement psts = con.prepareStatement(sql);
-        psts.setInt(1, userId);
-        ResultSet resultSet = psts.executeQuery();
-        while (resultSet.next()) {
-            int projectId = resultSet.getInt("projectId");
-            String projectName = resultSet.getString("projectName");
-            String roleTitle = resultSet.getString("RoleTitle");
-            Project project = new Project(projectName);
-            project.setProjectId(projectId);
-            project.setUserRole(roleTitle);
-            projects.add(project);
+    //    getProjects
+    public List<Project> getProjects(int userId) {
+        List<Project> projects = new ArrayList<>();
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+            String sql = "SELECT P.projectId, P.projectName, R.RoleTitle " +
+                    "FROM User_Project_Role UPR " +
+                    "JOIN Project P ON UPR.ProjectID = P.ProjectID " +
+                    "JOIN Role R ON UPR.RoleID = R.RoleID " +
+                    "WHERE UPR.UserID = ?";
+            PreparedStatement psts = con.prepareStatement(sql);
+            psts.setInt(1, userId);
+            ResultSet resultSet = psts.executeQuery();
+            while (resultSet.next()) {
+                int projectId = resultSet.getInt("projectId");
+                String projectName = resultSet.getString("projectName");
+                String roleTitle = resultSet.getString("RoleTitle");
+                Project project = new Project(projectName);
+                project.setProjectId(projectId);
+                project.setUserRole(roleTitle);
+                projects.add(project);
+            }
+        } catch (SQLException e) {
+            System.out.println("Database not connected");
+            e.printStackTrace();
         }
-    } catch (SQLException e) {
-        System.out.println("Database not connected");
-        e.printStackTrace();
+        return projects;
     }
-    return projects;
-}
-// Get subProjects
-public List<SubProject> getSubProjects(int projectId, String userRole) {
-    List<SubProject> subprojects = new ArrayList<>();
-    try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
-        String sql = "SELECT P.SubprojectId, P.SubprojectName, P.Hours, P.Deadline " +
-                "FROM Project_Subproject PS " +
-                "JOIN Subproject P ON PS.SubprojectId = P.SubprojectId " +
-                "WHERE PS.ProjectId = ?";
-        PreparedStatement psts = con.prepareStatement(sql);
-        psts.setInt(1, projectId);
-        ResultSet resultSet = psts.executeQuery();
-        while (resultSet.next()) {
-            int subprojectId = resultSet.getInt("SubprojectId");
-            String subprojectName = resultSet.getString("SubprojectName");
-            int hours = resultSet.getInt("Hours");
 
-            SubProject subproject = new SubProject(subprojectName, projectId);
-            Date deadline = resultSet.getDate("Deadline");
-            subproject.setDeadline(deadline);
-
-            subproject.setProjectId(subprojectId);
-            subproject.setHours(hours);
-
-            subproject.setParentProjectId(projectId);
-            subproject.setUserRole(userRole);
-
-            subprojects.add(subproject);
-        }
-    } catch (SQLException e) {
-        System.out.println("Database not connected");
-        e.printStackTrace();
-    }
-    return subprojects;
-}
-//    Create Project
-public void addNewProject(Project newProject) {
-    try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
-        String sql = "INSERT INTO Project (ProjectName) VALUES (?)";
-        PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        pstmt.setString(1, newProject.getProjectName());
-        pstmt.executeUpdate();
-
-        ResultSet rs = pstmt.getGeneratedKeys();
-        if (rs.next()) {
-            int projectId = rs.getInt(1);
-            newProject.setProjectId(projectId);
-        }
-    } catch (SQLException e) {
-        System.out.println("Error adding new project");
-        e.printStackTrace();
-    }
-}
-
-//    getProject
-public Project getProject(int projectId) {
-    Project project = null;
-    try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
-        String sql = "SELECT * FROM Project WHERE projectId = ?";
-        PreparedStatement psts = con.prepareStatement(sql);
-        psts.setInt(1, projectId);
-        ResultSet resultSet = psts.executeQuery();
-        if (resultSet.next()) {
-            String projectName = resultSet.getString("ProjectName");
-            project = new Project(projectName);
-            project.setProjectId(projectId);
-        } else {
-            // If project not found in Project table, check SubProject table
-            sql = "SELECT * FROM SubProject WHERE subProjectId = ?";
-            psts = con.prepareStatement(sql);
+    // Get subProjects
+    public List<SubProject> getSubProjects(int projectId, String userRole) {
+        List<SubProject> subprojects = new ArrayList<>();
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+            String sql = "SELECT P.SubprojectId, P.SubprojectName, Temp.Total AS Hours, P.Deadline " +
+                    "FROM Project_Subproject PS " +
+                    "JOIN Subproject P ON PS.SubprojectId = P.SubprojectId " +
+                    "LEFT JOIN (SELECT ST.SubProjectId, SUM(T.Hours) AS Total " +
+                    "           FROM Subproject_Task ST " +
+                    "           JOIN Task T ON ST.TaskId = T.TaskId " +
+                    "           GROUP BY ST.SubProjectId) AS Temp ON PS.SubprojectId = Temp.SubProjectId " +
+                    "WHERE PS.ProjectId = ?";
+            PreparedStatement psts = con.prepareStatement(sql);
             psts.setInt(1, projectId);
-            resultSet = psts.executeQuery();
+            ResultSet resultSet = psts.executeQuery();
+            while (resultSet.next()) {
+                int subprojectId = resultSet.getInt("SubprojectId");
+                String subprojectName = resultSet.getString("SubprojectName");
+                int hours = resultSet.getInt("Hours");
+
+                SubProject subproject = new SubProject(subprojectName, projectId);
+
+                Date deadline = resultSet.getDate("Deadline");
+                subproject.setDeadline(deadline);
+                subproject.setProjectId(subprojectId);
+                subproject.setHours(hours);
+                subproject.setParentProjectId(projectId);
+                subproject.setUserRole(userRole);
+
+                subprojects.add(subproject);
+            }
+        } catch (SQLException e) {
+            System.out.println("Database not connected");
+            e.printStackTrace();
+        }
+        return subprojects;
+    }
+
+    //    Create Project
+    public void addNewProject(Project newProject) {
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+            String sql = "INSERT INTO Project (ProjectName) VALUES (?)";
+            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, newProject.getProjectName());
+            pstmt.executeUpdate();
+
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                int projectId = rs.getInt(1);
+                newProject.setProjectId(projectId);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error adding new project");
+            e.printStackTrace();
+        }
+    }
+
+    //    getProject
+    public Project getProject(int projectId) {
+        Project project = null;
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+            String sql = "SELECT * FROM Project WHERE projectId = ?";
+            PreparedStatement psts = con.prepareStatement(sql);
+            psts.setInt(1, projectId);
+            ResultSet resultSet = psts.executeQuery();
             if (resultSet.next()) {
-                String projectName = resultSet.getString("SubProjectName");
+                String projectName = resultSet.getString("ProjectName");
                 project = new Project(projectName);
                 project.setProjectId(projectId);
+            } else {
+                // If project not found in Project table, check SubProject table
+                sql = "SELECT * FROM SubProject WHERE subProjectId = ?";
+                psts = con.prepareStatement(sql);
+                psts.setInt(1, projectId);
+                resultSet = psts.executeQuery();
+                if (resultSet.next()) {
+                    String projectName = resultSet.getString("SubProjectName");
+                    project = new Project(projectName);
+                    project.setProjectId(projectId);
+                }
             }
+        } catch (SQLException e) {
+            System.out.println("Project not located");
+            e.printStackTrace();
         }
-    } catch (SQLException e) {
-        System.out.println("Project not located");
-        e.printStackTrace();
+        return project;
     }
-    return project;
-}
 
 //add Subproject:
 
@@ -172,8 +176,8 @@ public Project getProject(int projectId) {
         }
         throw new SQLException("Failed to retrieve last inserted ID");
     }
-//    TODO  - getTasks
 
+    //    getTasks
     public List<Task> getTasks(int subProjectId, String userRole) {
         List<Task> tasks = new ArrayList<>();
         try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
@@ -188,7 +192,7 @@ public Project getProject(int projectId) {
                 int taskId = resultSet.getInt("TaskId");
                 String taskName = resultSet.getString("TaskName");
                 int hours = resultSet.getInt("Hours");
-                Task task = new Task(taskName, subProjectId);
+                Task task = new Task(taskName);
 
                 task.setTaskId(taskId);
                 task.setHours(hours);
@@ -202,8 +206,28 @@ public Project getProject(int projectId) {
         }
         return tasks;
     }
+    //    TODO  - getTask
+    public Task getTask(int taskId) {
+        Task task = null;
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+            String sql = "SELECT * FROM Task WHERE TaskId = ?";
+            PreparedStatement psts = con.prepareStatement(sql);
+            psts.setInt(1, taskId);
+            ResultSet resultSet = psts.executeQuery();
+            if (resultSet.next()) {
+                String taskName = resultSet.getString("TaskName");
+                int hours = resultSet.getInt("Hours");
+                task = new Task(taskName);
+                task.setTaskId(taskId);
+                task.setHours(hours);
 
-//    TODO  - getTask
+            }
+        } catch (SQLException e) {
+            System.out.println("Task not located");
+            e.printStackTrace();
+    }
+        return task;
+}
 //    TODO  - getUsers
 //    TODO  - getHoursTask
 //    TODO  - getHoursSubProject
