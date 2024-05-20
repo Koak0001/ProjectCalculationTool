5504f4a3e0e2e50d3f1e4ee6186dbc62a1246d6a
Almer Toro Coskun <alco0001@stud.kea.dk>
2024-05-20 10:00:56 +0200

    Getusers, resolves #39 & Getusers, resolves #53

--- src/main/java/projectcalculationtool/service/ProjectService.java
+++ src/main/java/projectcalculationtool/service/ProjectService.java
@@ -15,7 +15,6 @@ public class ProjectService {
     public ProjectService(ProjectRepository projectRepository) {this.projectRepository = projectRepository;}
 
 
-
     public void addProject(Project project) {
         projectRepository.addNewProject(project);}
     public void addSubProject(SubProject subProject, int parent) {
@@ -29,26 +28,26 @@ public class ProjectService {
 //     Call getProject
     public Project getProject (int projectId) {return projectRepository.getProject(projectId);}
 //   Call getSubprojects
-
     public List<SubProject> getSubProjects(int projectId, String role) {return projectRepository.getSubProjects(projectId, role);}
 // Call getSubProject
     public SubProject getSubProject(int subProjectId){return (SubProject) projectRepository.getSubProject(subProjectId);}
     public void updateSubProject(SubProject subProject) {projectRepository.updateSubProject(subProject);}
-
 //  Call verifyUser/login
     public void login(String username, String password) {projectRepository.login(username, password);}
-
     public User getLoggedInUser() {return projectRepository.getLoggedInUser();}
-
 //  Call getTasks
     public List<Task> getTasks(int projectId, String role) {return projectRepository.getTasks(projectId, role);}
 //  Call getTask
     public Task getTask (int taskId) {return projectRepository.getTask(taskId);}
     public void updateTask (Task task) {projectRepository.updateTask(task);}
+    //  Call getUsers
+    public List<User> getUsers() {return projectRepository.getUsers();}
+    // Call getUser
+    public User getUser(int userId) {return projectRepository.getUser(userId);}
+
 
 //    TODO - Call getUserSitePermissions
 //    TODO - Call createProject
-//    TODO - Call getUsers
 //    TODO - Call setRole
 //    TODO - Call getRole
 //    TODO - Call getCollaborators
